0143c195c79884757b4af8e426fa13b3dc4b8275
sadlee <sadlak.radoslaw@gmail.com>
2020-01-13 20:57:48 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://protected-inlet-25381.herokuapp.com/v1/task/';
+  var apiRoot = 'https://git.heroku.com/still-basin-24783.git/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
